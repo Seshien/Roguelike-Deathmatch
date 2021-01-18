1cc6d80bb729e5f314ecce4ddcd91e87c9907d97
soulrpg <soulrpg@users.noreply.github.com>
2021-01-18 20:52:08 +0100

    PlayerInfo + class files added

--- Client RLD/src/PlayerInfo.h
+++ Client RLD/src/PlayerInfo.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 class PlayerInfo
 {
+	int playerId;
+	int prevxPos;
+	int prevyPos;
+	int xPos;
+	int yPos;
+	int killCount;
+
+	// Dla animowania/interpolacji w kliencie pomiedzy ruchami
+	double realXPos;
+	double realYPos;
+
+	bool isAlive;
+
+	std::vector<std::shared_ptr<sf::Texture>> textureSet;
+
+public:
+	PlayerInfo(int playerId, int x, int y, std::vector<std::shared_ptr<sf::Texture>> textureSet, int killCount);
+	// Sets realXPos and realYPos for given frame 
+	void interpolate(double time, double maxTime);
+	void setNewPosition(int x, int y);
+	void draw(sf::RenderWindow& window);
+	std::string getFacing();
 };
 
